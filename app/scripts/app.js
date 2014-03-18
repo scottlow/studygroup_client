37a8cd243f161b86e64c3177a4b5cb6fd758ab47
scottlow <slow@uvic.ca>
2014-03-18 08:48:21 -0700

    Cleaned up login a bit so we redirect to dashboard if you're already logged in without cluttering the back stack.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -19,7 +19,7 @@ angular.module('studygroupClientApp', [
       controller: 'MainCtrl',
       authenticate: false
     })
-    .state("dashboard", {
+    .state('dashboard', {
       url: '/dashboard',
       templateUrl: 'views/dashboard.html',
       controller: 'DashboardCtrl',
@@ -28,10 +28,15 @@ angular.module('studygroupClientApp', [
     $urlRouterProvider.otherwise("/");    
   })
   .run(function ($rootScope, $state, AuthService) {
-    $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+      if(toState.url === '/') {
+        // We are hitting the root of the page. If this is happeneing, we should check to see if the user has the cookie set to login.
+        $state.transitionTo(AuthService.isAuthenticated() ? 'dashboard' : 'main', null, {location: 'replace'});
+        event.preventDefault();
+      }
       if (toState.authenticate && !AuthService.isAuthenticated()){
         // User isn’t authenticated
-        $state.transitionTo("main");
+        $state.transitionTo('main');
         event.preventDefault(); 
       }
     });
