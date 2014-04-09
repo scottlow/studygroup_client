1320147fe0637561199b5bbf48167a842d95fda7
jguze <jaguze@gmail.com>
2014-04-08 23:57:32 -0700

    Adding in non disappearing session panels

--- app/scripts/directives/sessionpanel.js
+++ app/scripts/directives/sessionpanel.js
@@ -12,18 +12,26 @@ angular.module('studygroupClientApp')
       controller: ['$scope', function($scope) {
         $scope.selectedCourses = StateService.getSelectedCourses();
         $scope.sessions = [];
-        $scope.getAvailableSessions = function() {
-            // This pane might load faster than the courses are set. If there are no courses, wait a
-            // few seconds. If there's still none, show the empty page (TODO)
-            $scope.sessions = [];
+        $scope.hostSessions = [];
+        $scope.viewSessions = [];
+        $scope.getAvailableSessions = function(values) {
+            var oldSessions = $scope.sessions.slice();
             var url = "id=";
-            angular.forEach($scope.selectedCourses, function(value) {
+            angular.forEach(values, function(value) {
                 url = url + value.id + "&id=";
             });
             url = url.substring(0, url.length - 4);
             return $http.get('http://localhost:8000/' + 'sessions/courses/?' + url).success(function(data) {
+                oldSessions.forEach(function(element, index) {
+                    // If this no longer exists in the data
+                    var dataIndex = data.indexOf(element);
+                    if (dataIndex > -1) {
+                        $scope.sessions.splice(index, 1);
+                        data.splice(dataIndex, 1);
+                    }
+                });
                 angular.forEach(data, function(value) {
-                    $scope.sessions.push(value);
+                        $scope.sessions.push(value);
                 });
             });
         };
