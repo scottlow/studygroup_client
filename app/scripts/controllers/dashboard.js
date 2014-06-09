1df965918dac5e15e4e4b4e9204e35716fa8d1da
scottlow <slow@uvic.ca>
2014-06-09 16:29:29 -0700

    Added the UI for the edit profile modal.
    The back end is not hooked up yet. That’s coming soon!

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -2,6 +2,11 @@
 
 angular.module('dashboardControllers', [])
   .controller('DashboardCtrl', function ($scope, $http, AuthService, StateService, $angularCacheFactory) {
+
+    $scope.showChangePassword = false;
+    $scope.newPassword = "";
+    $scope.verifyPassword = "";
+
     // Log the user out
     $scope.logout = function() {
       StateService.logout();
@@ -16,7 +21,29 @@ angular.module('dashboardControllers', [])
     // This is $broadcasted by StateService when a user has successfully logged in.
     // Here we grab the user's first name (or username if they didn't enter a name) for displaying in the top bar.
     $scope.$on('loginProcessed', function(){
+      $scope.user = StateService.getUserObj();      
       $scope.full_name = StateService.getUsername();
       $scope.first_name = $scope.full_name.split(' ')[0];
     });
+
+    $scope.checkPasswords = function() {
+      if($scope.newPassword !== $scope.verifyPassword) {
+        $scope.updateUserForm.verifyPassword.$error.passwordMatch = true;
+        $scope.updateUserForm.newPassword.$invalid = true;          
+        $scope.updateUserForm.verifyPassword.$invalid = true;
+      } else if($scope.newPassword !== "" && $scope.verifyPassword !== "") {
+        $scope.updateUserForm.verifyPassword.$error.passwordMatch = false;
+        $scope.updateUserForm.newPassword.$invalid = false;            
+        $scope.updateUserForm.verifyPassword.$invalid = false;          
+      }
+    };
+
+    $scope.submitAccountUpdate = function() {
+      if($scope.showChangePassword) {
+        $scope.checkPasswords();
+      }
+
+      $scope.hasSubmitted = true;      
+    };
+
   });
