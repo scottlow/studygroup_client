3eba3f3b67c75e75141251148743bae9b7c19a8f
Bill Xiong <billxiong99@gmail.com>
2014-10-16 12:04:26 -0700

    Integrated ics.js to generate calendar objects. Dummy data only right now.

--- app/scripts/directives/sessioncard.js
+++ app/scripts/directives/sessioncard.js
@@ -25,7 +25,12 @@ angular.module('studygroupClientApp')
 
         $scope.addSessionToCalendar = function(sessionID) {
           StateService.addToCalendar(parseInt(sessionID));
+          StateService.iCalObj = ics();
+          StateService.iCalObj.addEvent('Demo Event', 'This is thirty minut event', 'Nome, AK', '8/7/2013 5:30 pm', '8/9/2013 6:00 pm');
           console.log("Generating Calendar Object...");
+          return StateService.iCalObj.download();
+
+
         }
 
       }],
